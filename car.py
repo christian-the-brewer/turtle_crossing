eb4dd57a60948e65254aff4a1be9b05837340adf
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-23 12:39:56 -0500

    adds move function for car class

--- car.py
+++ car.py
@@ -1,8 +1,23 @@
 from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 class Car(Turtle):
-    def __init__(self, position, speed):
+    def __init__(self, y, speed, direction):
         super().__init__()
+        self.color(random.choice(COLORS))
+        self.shape("square")
+        self.shapesize(stretch_len=40)
+        self.penup()
+        self.speed = speed
+        self.direction = direction
+        self.y = y
+        self.goto((300 * direction) * -1, position)
+
+
+    def move(self):
+        if self.xcor() > 350 or self.xcor() < -350:
+           self.goto((300 * self.direction) * -1, self.y) 
+        self.fd(self.speed * self.direction)
